3ac4ca36413994527c906ab890bbd21993caea0d
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2024-01-11 17:02:11 -0500

    Created UserProfileService

---
+++ JH/src/main/java/UserProfileService.java
@@ -0,0 +1,48 @@
+import com.mycompany.myapp.domain.UserProfile;
+import com.mycompany.myapp.repository.UserProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserProfileService {
+
+    private UserProfileRepository repository;
+
+    @Autowired
+    public UserProfileService(UserProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    public UserProfile create(UserProfile userProfile) {
+        return repository.save(userProfile);
+    }
+
+    public UserProfile readById(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public List<UserProfile> readAll() {
+        Iterable<UserProfile> allUserProfile = repository.findAll();
+        List<UserProfile> userProfileList = new ArrayList<>();
+        allUserProfile.forEach(userProfileList::add);
+        return userProfileList;
+    }
+
+    public UserProfile update(Long id, UserProfile newUserProfileData) {
+        UserProfile userProfileInDatabase = this.readById(id);
+        userProfileInDatabase.setFirstName(newUserProfileData.getFirstName());
+        userProfileInDatabase.setLastName(newUserProfileData.getLastName());
+        userProfileInDatabase.setUserName(newUserProfileData.getUserName());
+        userProfileInDatabase = repository.save(userProfileInDatabase);
+        return userProfileInDatabase;
+    }
+
+    public UserProfile deleteById(Long id) {
+        UserProfile userProfileToBeDeleted = this.readById(id);
+        repository.delete(userProfileToBeDeleted);
+        return userProfileToBeDeleted;
+    }
+}
--- JH/src/main/java/com/mycompany/myapp/config/CommentConfig.java
+++ JH/src/main/java/com/mycompany/myapp/config/CommentConfig.java
@@ -5,37 +5,41 @@ import com.mycompany.myapp.domain.UserProfile;
 import com.mycompany.myapp.domain.Video;
 import com.mycompany.myapp.repository.CommentRepository;
 import com.mycompany.myapp.repository.UserProfileRepository;
+import com.mycompany.myapp.repository.VideoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class CommentConfig {
 
     @Bean
     public CommandLineRunner populateComments(CommentRepository commentRepository,
-                                              UserProfileRepository userProfileRepository,
-                                              VideoConfig videoConfig) {
+                                              VideoRepository videoRepository,
+                                              UserProfileRepository userProfileRepository) {
         return args -> {
-            // Retrieve sample user profile (assuming it already exists)
-            UserProfile selectedUserProfile = userProfileRepository.findById(1L).orElse(null);
+            // Retrieve sample video and user profiles (assuming they already exist)
+            Video sampleVideo = videoRepository.findById(1L).orElse(null);
+            UserProfile sampleUserProfile = userProfileRepository.findById(1L).orElse(null);
 
-            if (selectedUserProfile != null) {
-                // Retrieve list of sample videos from VideoConfig
-                List<Video> sampleVideos = videoConfig.getSampleVideos();
+            if (sampleVideo != null && sampleUserProfile != null) {
+                // Add comment data to the database
+                Comment comment1 = new Comment();
+                comment1.setPost("I wish the Southern Classic was still around! It's also cool that 02' alumni can say they were in this movie. ");
+                comment1.setThumbsUp(1);
+                comment1.setVideo(sampleVideo);
+                comment1.setPostedBy(sampleUserProfile);
 
-                // Add comment data to the database for each video
-                for (Video sampleVideo : sampleVideos) {
-                    Comment comment = new Comment();
-                    comment.setPost("Great video!");
-                    comment.setThumbsUp(10);
-                    comment.setVideo(videoConfi);
-                    comment.setPostedBy(selectedUserProfile);
+                Comment comment2 = new Comment();
+                comment2.setPost("Interesting discussion.");
+                comment2.setThumbsUp(5);
+                comment2.setVideo(sampleVideo);
+                comment2.setPostedBy(sampleUserProfile);
 
-                    commentRepository.save(comment);
-                }
+                commentRepository.save(comment1);
+                commentRepository.save(comment2);
+
+                // Add more comments as needed
             }
         };
     }
