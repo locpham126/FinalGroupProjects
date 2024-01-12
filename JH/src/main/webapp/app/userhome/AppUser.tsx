65f104a549f7bb6bbf55f28a16a0ea0c5fdf0e1a
Loc Pham <148402164+locpham126@users.noreply.github.com>
2024-01-12 04:03:12 -0500

    working on getting rows of movies

--- JH/src/main/webapp/app/userhome/AppUser.tsx
+++ JH/src/main/webapp/app/userhome/AppUser.tsx
@@ -3,7 +3,8 @@ import './AppUser.scss';
 import {Translate} from "react-jhipster";
 import {useLocation, useNavigate} from "react-router-dom";
 import {useAppDispatch} from "app/config/store";
-import {Row} from "reactstrap";
+import Row from "./Row";
+import requests from "./requests";
 
 //page to reroute login.users
 function AppUser(){
@@ -22,9 +23,9 @@ function AppUser(){
       <h1>
         Howdy this is the User Homepage
       </h1>
-      <Row title = "Action"/>
-      <Row title = "Romance"/>
-      <Row title = "Comedy"/>
+      <Row title = "Action" fetchUrl={requests.fetchAction} />
+      <Row title = "Romance" fetchUrl={requests.fetchRomance}/>
+      <Row title = "Comedy" fetchUrl={requests.fetchComedy}/>
     </div>
     </>
   );
--- JH/src/main/webapp/app/userhome/Row.tsx
+++ JH/src/main/webapp/app/userhome/Row.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from './axios';
 
-function Row() {
+
+function Row( {title, fetchUrl} ) {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchData(){
+      const request = await axios.get(fetchUrl);
+    //   'http://localhost:8080'
+      setVideos(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
   return(
-    <div>
-
+    <div className = "row">
+      {/*   title */}
+      <h2>{title}</h2>
+      <div className="row_names">
+        {videos.map(video => (
+          <img src={video.id} alt={video.title}/>
+        ))}
+      </div>
+      {/* containt -> posters */}
       </div>
   )
 }
