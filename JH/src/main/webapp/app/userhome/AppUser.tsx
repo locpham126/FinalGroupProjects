d90a1be22ff05fd15f855d1fae793d29f6c0597d
Loc Pham <148402164+locpham126@users.noreply.github.com>
2024-01-13 01:41:52 -0500

    changed eslintrc

--- JH/src/main/webapp/app/userhome/AppUser.tsx
+++ JH/src/main/webapp/app/userhome/AppUser.tsx
@@ -5,6 +5,7 @@ import {useLocation, useNavigate} from "react-router-dom";
 import {useAppDispatch} from "app/config/store";
 import Row from "./Row";
 import requests from "./requests";
+import Rows from "app/userhome/Rows";
 
 //page to reroute login.users
 function AppUser(){
@@ -21,23 +22,23 @@ function AppUser(){
     <>
     <div className ="AppUser">
       <h1>
-        Howdy this is the User Homepage
+        Howdy! Welcome to VLA (Netflix Clone)
       </h1>
-      <Row title = "Action" fetchUrl={requests.fetchAction} />
-      <Row title = "Romance" fetchUrl={requests.fetchRomance}/>
-      <Row title = "Comedy" fetchUrl={requests.fetchComedy}/>
-      <Row title = "Fantasy" fetchUrl={requests.fetchAction} />
-      <Row title = "Animation" fetchUrl={requests.fetchRomance}/>
-      <Row title = "Horror" fetchUrl={requests.fetchComedy}/>
-      <Row title = "Sci-Fi" fetchUrl={requests.fetchAction} />
-      <Row title = "Drama" fetchUrl={requests.fetchRomance}/>
-      <Row title = "Adventure" fetchUrl={requests.fetchComedy}/>
-      <Row title = "Crime" fetchUrl={requests.fetchAction} />
-      <Row title = "Thriller" fetchUrl={requests.fetchRomance}/>
-      <Row title = "Music" fetchUrl={requests.fetchComedy}/>
-      <Row title = "Family" fetchUrl={requests.fetchAction} />
-      <Row title = "Sports" fetchUrl={requests.fetchRomance}/>
-      <Row title = "Biography" fetchUrl={requests.fetchComedy}/>
+      <Rows title = "Action" fetchUrl={requests.fetchAction} />
+      <Rows title = "Romance" fetchUrl={requests.fetchRomance}/>
+      <Rows title = "Comedy" fetchUrl={requests.fetchComedy}/>
+      <Rows title = "Fantasy" fetchUrl={requests.fetchAction} />
+      <Rows title = "Animation" fetchUrl={requests.fetchRomance}/>
+      <Rows title = "Horror" fetchUrl={requests.fetchComedy}/>
+      <Rows title = "Sci-Fi" fetchUrl={requests.fetchAction} />
+      <Rows title = "Drama" fetchUrl={requests.fetchRomance}/>
+      <Rows title = "Adventure" fetchUrl={requests.fetchComedy}/>
+      <Rows title = "Crime" fetchUrl={requests.fetchAction} />
+      <Rows title = "Thriller" fetchUrl={requests.fetchRomance}/>
+      <Rows title = "Music" fetchUrl={requests.fetchComedy}/>
+      <Rows title = "Family" fetchUrl={requests.fetchAction} />
+      <Rows title = "Sports" fetchUrl={requests.fetchRomance}/>
+      <Rows title = "Biography" fetchUrl={requests.fetchComedy}/>
     </div>
     </>
   );
--- JH/src/main/webapp/app/userhome/axios.js
+++ JH/src/main/webapp/app/userhome/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTcwNTEyNjkyMSwiYXV0aCI6IlJPTEVfQURNSU4gUk9MRV9VU0VSIiwiaWF0IjoxNzA1MDQwNTIxfQ.hn5Oma6blpCxN2uHBfOhHCQpkdjEzQJ44t33cLKLSy3CtRrFgeNRTMXK_pLwRTejf5BWz6obGmCHW0ztNpfw4A'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:9000'
+  baseURL: 'http://localhost:8080',
+  headers:{
+    accept: '*/*',
+    // Authorization: `Bearer ${token}`
+  }
 });
 
 export default instance;
